7db1bd0a6dc346999c25fe4f3d5fc9fb6c5f2eb1
Elyas Bahodi <10201007@ucn.dk>
2021-10-11 09:40:23 +0200

    hello

---
+++ S3/Assignment2/basics.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * How to concatenate an integer, string and float together, the basics.
+ *  */
+$myInt = 15;
+$myFloat = 24.0;
+$myString = 'sup';
+
+echo $myInt + $myFloat;
+
+echo "\n\n";
+
+echo $myString . $myInt . $myFloat;
+
+
+?>
